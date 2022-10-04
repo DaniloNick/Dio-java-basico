eda3af747c8d0fe907e45117b3402fc9e2edd63f
DaniloNick <daniloageu@gmail.com>
2022-10-04 01:39:43 -0300

    acrescentando scanner para entrada de dados

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class App{
    public static void main(String[] args) {
-        System.out.println("Teste de commit");
+    Scanner sc = new Scanner(System.in);
+        System.out.println("Digite seu nome:");
+String nome = sc.nextLine();
+System.out.println("Bem vindo " + nome);
 
+        sc.close();
     }
 }
